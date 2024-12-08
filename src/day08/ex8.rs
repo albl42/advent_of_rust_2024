e613492d5fa45f72ddfb8306f1cd68f1780b4e3b
Marcel Albl <marcel.albl@cudos.ch>
2024-12-08 23:24:38 +0100

    Refactored advent of code day 8.

--- src/day08/ex8.rs
+++ src/day08/ex8.rs
@@ -22,22 +22,36 @@ pub fn parse_input(input: &str) -> Vec<Vec<char>> {
 //     }
 // }
 
-pub fn parse_map(input: &Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
-    input
-        .into_iter()
-        .enumerate()
-        .flat_map(|(x, row)| {
-            row.into_iter()
-                .enumerate()
-                .filter_map(move |(y, &cell)| match cell.is_alphanumeric() {
-                    true => Some((cell, (x, y))),
-                    false => None,
-                })
-        })
-        .fold(HashMap::new(), |mut acc, (k, v)| {
-            acc.entry(k).or_insert_with(Vec::new).push(v);
-            acc
-        })
+// pub fn parse_map(input: &Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
+//     input
+//         .iter()
+//         .enumerate()
+//         .flat_map(|(x, row)| {
+//             row.iter()
+//                 .enumerate()
+//                 .filter_map(move |(y, &cell)| match cell.is_alphanumeric() {
+//                     true => Some((cell, (x, y))),
+//                     false => None,
+//                 })
+//         })
+//         .fold(HashMap::new(), |mut acc, (k, v)| {
+//             acc.entry(k).or_insert_with(Vec::new).push(v);
+//             acc
+//         })
+// }
+
+pub fn parse_map(input: &[Vec<char>]) -> HashMap<char, Vec<(usize, usize)>> {
+    let mut map = HashMap::new();
+
+    for (x, row) in input.iter().enumerate() {
+        for (y, &cell) in row.iter().enumerate() {
+            if cell.is_alphanumeric() {
+                map.entry(cell).or_insert_with(Vec::new).push((x, y));
+            }
+        }
+    }
+
+    map
 }
 
 // pub fn antenna_positions(map: &HashMap<char, Vec<(usize, usize)>>) -> HashSet<(usize, usize)> {
@@ -47,103 +61,96 @@ pub fn parse_map(input: &Vec<Vec<char>>) -> HashMap<char, Vec<(usize, usize)>> {
 pub fn antenna_pairs(antennas: &Vec<(usize, usize)>) -> Vec<((usize, usize), (usize, usize))> {
     antennas
         .iter()
-        .flat_map(|a| {
-            antennas.iter().filter_map(move |b| match a != b {
-                true => Some((*a, *b)),
-                false => None,
-            })
-        })
+        .enumerate()
+        .flat_map(|(i, a)| antennas.iter().skip(i + 1).map(|b| (*a, *b)))
         .collect()
 }
 
-pub fn try_get_pole(a: i32, b: i32, limit: &(usize, usize)) -> Option<(usize, usize)> {
-    match a >= 0 && b >= 0 && a < limit.0 as i32 && b < limit.1 as i32 {
-        true => Some((a as usize, b as usize)),
-        false => None,
+pub fn try_get_pole(
+    pos: &(usize, usize),
+    step: &(i32, i32),
+    limit: &(usize, usize),
+) -> Option<(usize, usize)> {
+    let x = pos.0 as i32 + step.0;
+    let y = pos.1 as i32 + step.1;
+    if x >= 0 && y >= 0 && x < limit.0 as i32 && y < limit.1 as i32 {
+        Some((x as usize, y as usize))
+    } else {
+        None
     }
 }
 
 pub fn antinode_primary_poles(
     (a, b): &((usize, usize), (usize, usize)),
-    limit: &(usize, usize),
+    limit: (usize, usize),
 ) -> Vec<Option<(usize, usize)>> {
-    let diff = (b.0 as i32 - a.0 as i32, b.1 as i32 - a.1 as i32);
-    let north = try_get_pole(a.0 as i32 - diff.0, a.1 as i32 - diff.1, limit);
-    let south = try_get_pole(b.0 as i32 + diff.0, b.1 as i32 + diff.1, limit);
+    let step_down = (b.0 as i32 - a.0 as i32, b.1 as i32 - a.1 as i32);
+    let step_up = (-step_down.0, -step_down.1);
+
+    let north = try_get_pole(a, &step_up, &limit);
+    let south = try_get_pole(b, &step_down, &limit);
+
     vec![north, south]
 }
 
 pub fn antinode_all_poles(
     (a, b): &((usize, usize), (usize, usize)),
-    limit: &(usize, usize),
+    limit: (usize, usize),
 ) -> Vec<Option<(usize, usize)>> {
-    let diff = (b.0 as i32 - a.0 as i32, b.1 as i32 - a.1 as i32);
+    let step_down = (b.0 as i32 - a.0 as i32, b.1 as i32 - a.1 as i32);
+    let step_up = (-step_down.0, -step_down.1);
 
     let mut result = vec![];
-    let mut north = try_get_pole(b.0 as i32 - diff.0, b.1 as i32 - diff.1, limit);
-    while north.is_some() {
-        result.push(north);
-        north = try_get_pole(
-            north.unwrap().0 as i32 - diff.0,
-            north.unwrap().1 as i32 - diff.1,
-            limit,
-        );
+
+    // Starting at end positions 'b' in order to also include start position 'a'
+    let mut north = try_get_pole(b, &step_up, &limit);
+    while let Some(n) = north {
+        result.push(Some(n));
+        north = try_get_pole(&n, &step_up, &limit);
     }
-    let mut south = try_get_pole(a.0 as i32 + diff.0, a.1 as i32 + diff.1, limit);
-    while south.is_some() {
-        result.push(south);
-        south = try_get_pole(
-            south.unwrap().0 as i32 + diff.0,
-            south.unwrap().1 as i32 + diff.1,
-            limit,
-        );
+
+    // Starting at start positions 'a' in order to also include end position 'b'
+    let mut south = try_get_pole(a, &step_down, &limit);
+    while let Some(s) = south {
+        result.push(Some(s));
+        south = try_get_pole(&s, &step_down, &limit);
     }
+
     result
 }
 
-pub fn antinode_primary_positions(
+pub fn antinode_positions<G>(
     antennas: &Vec<(usize, usize)>,
-    limit: &(usize, usize),
-) -> Vec<(usize, usize)> {
-    let pairs = antenna_pairs(&antennas);
-    pairs
+    gen_func: &mut G,
+) -> Vec<(usize, usize)>
+where
+    G: FnMut(&((usize, usize), (usize, usize))) -> Vec<Option<(usize, usize)>>,
+{
+    antenna_pairs(&antennas)
         .iter()
-        .flat_map(|x| antinode_primary_poles(x, limit))
+        .flat_map(gen_func)
         .flatten()
         .collect()
 }
 
-pub fn antinode_all_positions(
-    antennas: &Vec<(usize, usize)>,
-    limit: &(usize, usize),
-) -> Vec<(usize, usize)> {
-    let pairs = antenna_pairs(&antennas);
-    pairs
-        .iter()
-        .flat_map(|x| antinode_all_poles(x, limit))
-        .flatten()
+pub fn valid_antinode_positions<G>(
+    antenna_map: &HashMap<char, Vec<(usize, usize)>>,
+    gen_func: &mut G,
+) -> Vec<(usize, usize)>
+where
+    G: FnMut(&((usize, usize), (usize, usize))) -> Vec<Option<(usize, usize)>>,
+{
+    antenna_map
+        .values()
+        .flat_map(|x| antinode_positions(x, gen_func))
         .collect()
 }
 
-pub fn valid_primary_antinode_positions(
-    antenna_map: &HashMap<char, Vec<(usize, usize)>>,
-    limit: &(usize, usize),
-) -> Vec<(usize, usize)> {
-    antenna_map
-        .values()
-        .flat_map(|x| antinode_primary_positions(x, limit))
-        .collect::<HashSet<_>>()
-        .into_iter()
-        .collect()
-}
-
-pub fn valid_all_antinode_positions(
-    antenna_map: &HashMap<char, Vec<(usize, usize)>>,
-    limit: &(usize, usize),
-) -> Vec<(usize, usize)> {
-    antenna_map
-        .values()
-        .flat_map(|x| antinode_all_positions(x, limit))
+pub fn unique_elements<T>(vec: Vec<T>) -> Vec<T>
+where
+    T: std::hash::Hash + Eq,
+{
+    vec.into_iter()
         .collect::<HashSet<_>>()
         .into_iter()
         .collect()
@@ -152,13 +159,28 @@ pub fn valid_all_antinode_positions(
 pub fn part_one(input: &str) -> u64 {
     let grid = parse_input(input);
     let map = parse_map(&grid);
-    valid_primary_antinode_positions(&map, &(grid.len(), grid[0].len())).len() as u64
+
+    // Higher-Ranked Trait Bounds (HRTB) and Dynamic Dispatch
+    // Explicit &mut dyn for<'a> FnMut
+    // explicit higher-ranked trait bound (for<'a>), ensuring that the closure is compatible with any lifetime.
+    let mut gen_func: &mut dyn for<'a> FnMut(
+        &'a ((usize, usize), (usize, usize)),
+    ) -> Vec<Option<(usize, usize)>> =
+        &mut |pair| antinode_primary_poles(pair, (grid.len(), grid[0].len()));
+
+    unique_elements(valid_antinode_positions(&map, &mut gen_func)).len() as u64
 }
 
 pub fn part_two(input: &str) -> u64 {
     let grid = parse_input(input);
     let map = parse_map(&grid);
-    valid_all_antinode_positions(&map, &(grid.len(), grid[0].len())).len() as u64
+
+    let mut gen_func: &mut dyn for<'a> FnMut(
+        &'a ((usize, usize), (usize, usize)),
+    ) -> Vec<Option<(usize, usize)>> =
+        &mut |pair| antinode_all_poles(pair, (grid.len(), grid[0].len()));
+
+    unique_elements(valid_antinode_positions(&map, &mut gen_func)).len() as u64
 }
 
 pub fn exec(input: &Path) -> () {
